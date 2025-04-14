c50c4e176e6fecdd908c2b11f9065db4e9d95978
unknown <likhonsorkar002@gmail.com>
2025-04-14 10:44:05 +0600

    Create the Student class

--- student_database.py
+++ student_database.py
@@ -3,6 +3,13 @@ class StudentDatabase:
     @classmethod
     def add_student(self,student):
         self.student_list.append(student)
+class Student:
+    def __init__(self,student_id,name,department,is_enrolled):
+        self.student_id = student_id
+        self.name = name
+        self.department = department
+        self.is_enrolled = is_enrolled 
+        
 StudentDatabase.add_student("Likhon")
 StudentDatabase.add_student("Jahidul")
 
